9e3c1d50f1338fea2e00de1b7d516dedac3b3d1f
Liton Arefin <litonice09@gmail.com>
2018-06-04 02:09:43 +0600

    initialized plugin

---
+++ ultimate-amp.php
@@ -0,0 +1,11 @@
+<?php
+/* Plugin Name: Ultimate AMP
+ * Description: Ultimate Accelerated Mobile Pages WordPress Plugin
+ * Version: 1.0.0
+ * Author: Liton Arefin
+ * Author URI: https://jeweltheme.com/shop/ultimate-amp/
+ * Tags: amp, wp amp, google amp, amp project
+ * Text Domain: uamp
+ * Domain Path: /languages/
+ * License: GPLv2 or later
+ */
