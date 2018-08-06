f39b4b0196b49170d841fc518c05cd0238bbd52b
Liton Arefin <litonice09@gmail.com>
2018-08-07 03:42:30 +0600

    welcome message partially done

--- inc/Welcome.php
+++ inc/Welcome.php
@@ -12,4 +12,8 @@
 	<p>Building the future web, together</p>
 	<br>
 	<h2>Save Permalinks</h2>
-	<p>You need to <strong>Save Permalinks</strong> <a href="">Change Permalink</a> to <code>%post_name%</code></p>
+
+	<p>You need to <strong>Save Permalinks</strong> from <em> Settings>Permalinks Menu</em>. Or, Click to <a href="<?php echo
+        admin_url('options-permalink.php');?>">Change Permalink</a> to
+        <code><?php echo get_bloginfo('url');?>/%post_name%</code></p>
+    <p>Your Present Permalink is <code><?php echo get_bloginfo('url');?><?php echo get_option('permalink_structure');?></code></p>
